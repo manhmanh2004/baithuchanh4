2992b2e377f9f3a14f9fbc06a737ddec30d2eb96
manh2k4manh <manhmanhdo00@gmail.com>
2024-12-24 23:39:57 +0700

    phan migrate và seeder

--- library-management/database/seeders/BookSeeder.php
+++ library-management/database/seeders/BookSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as FakerFactory;
+use App\Models\Book;
 class BookSeeder extends Seeder
 {
     /**
@@ -12,6 +13,16 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = FakerFactory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Book::create([
+                'name' => $faker->unique()->sentence(3), // Generate unique book titles
+                'author' => $faker->name,
+                'category' => $faker->randomElement(['Fiction', 'Non-Fiction', 'Science Fiction', 'History', 'Biography']),
+                'year' => $faker->year,
+                'quantity' => $faker->numberBetween(1, 10),
+            ]);
+        }
     }
 }
--- library-management/database/seeders/DatabaseSeeder.php
+++ library-management/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(
+            // BookSeeder::class,
+            // ReaderSeeder::class,
+            BorrowSeeder::class,
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
