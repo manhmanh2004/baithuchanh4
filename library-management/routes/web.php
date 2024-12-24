062aa898c32cc7e34c3f6fe686b761510e787265
manh2k4manh <manhmanhdo00@gmail.com>
2024-12-24 23:46:46 +0700

    phần routes cập nhật

--- library-management/routes/web.php
+++ library-management/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowController;
+use App\Http\Controllers\ReaderController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [BookController::class, 'index'])->name('books.index');
+Route::resource('books', BookController::class);
+Route::resource('readers', ReaderController::class);
+Route::resource('borrows', BorrowController::class);
+
+
+Route::get('/borrows/history/{readerId}', [BorrowController::class, 'showHistory'])->name('borrows.history');
